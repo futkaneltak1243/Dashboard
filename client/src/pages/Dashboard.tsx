0130d4341036c27c03682388d71aa910f0ee2b88
futkaneltak1243 <ft.142001@gmail.com>
2025-10-12 09:46:31 +0300

    feat(server): create dashboard API endpoint

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,16 +1,48 @@
 import { StatGrid, Chart } from "../components/analytics"
 import { Calendar, Package, SquareMenu, UsersRound } from "lucide-react"
+import useFetch from "../hooks/useFetch/useFetch"
+
+
+type Data = {
+    users: number;
+    orders: number;
+    products: number;
+    exhibitions: number;
+}
 
 const Dashboard = () => {
+
+
+    const { error, loading, data } = useFetch<Data>('/dashboard')
+
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-500"></div>
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div className="flex flex-col items-center justify-center h-screen">
+                <p className="text-red-600 text-2xl mb-2">Oops! Something went wrong.</p>
+                <p className="text-gray-600">{error}</p>
+            </div>
+        );
+    }
+
+    if (!data) return null
+
     return (
         <div className="p-[15px] md:p-[30px]">
             <h1 className="text-text-light dark:text-text-dark text-3xl">Dashboard</h1>
             <div className="mt-[27px]">
                 <StatGrid>
-                    <StatGrid.StatCard title="Total Users" value="40,689" direction="up" percent={1.5} lastDifference="yesterday" iconColor="blue" Icon={UsersRound} />
-                    <StatGrid.StatCard title="Total Products" value="40,689" direction="up" percent={2.5} lastDifference="last month" iconColor="green" Icon={Package} />
-                    <StatGrid.StatCard title="Total Orders" value="40,689" direction="down" percent={2.5} lastDifference="yesterday" iconColor="orange" Icon={SquareMenu} />
-                    <StatGrid.StatCard title="Total Exhibitions" value="40,689" direction="down" percent={34.7} lastDifference="last year" iconColor="yellow" Icon={Calendar} />
+                    <StatGrid.StatCard title="Total Users" value={String(data.users)} direction="up" percent={1.5} lastDifference="yesterday" iconColor="blue" Icon={UsersRound} />
+                    <StatGrid.StatCard title="Total Products" value={String(data.products)} direction="up" percent={2.5} lastDifference="last month" iconColor="green" Icon={Package} />
+                    <StatGrid.StatCard title="Total Orders" value={String(data.orders)} direction="down" percent={2.5} lastDifference="yesterday" iconColor="orange" Icon={SquareMenu} />
+                    <StatGrid.StatCard title="Total Exhibitions" value={String(data.exhibitions)} direction="down" percent={34.7} lastDifference="last year" iconColor="yellow" Icon={Calendar} />
 
                 </StatGrid>
             </div>
